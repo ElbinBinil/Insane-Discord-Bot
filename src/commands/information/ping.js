15fd9ce30e63d4e8103703e775b4ea04db22f5d6
Elbin Binil <elbinbinl@gmail.com>
2021-08-30 13:25:42 +0530

    Added example command

--- src/commands/information/ping.js
+++ src/commands/information/ping.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ping")
-    .setDescription("This is a desciption"),
+    .setDescription("Example Command"),
   async execute(interaction) {
     await interaction.reply("Pong!");
   },
